43a2d5dd3ac5023f6a3dd806267dc0f0db5bf007
Uenderss <meajudabarto@gmail.com>
2022-07-03 00:53:10 -0300

    criar index e pasta components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Header } from './header';
+import {Footer} from './footer';
+import {Main}from './main';
+
+
+const App =()=>{
+    return <div>
+            
+            <Header/>
+            <Main></Main>
+            <Footer/>
+        </div>;
+};
+
+export default App;
