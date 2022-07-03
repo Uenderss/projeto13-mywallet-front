b9bc47fdb2a57063288fdf293cfd2d439657a8e4
Uenderss <meajudabarto@gmail.com>
2022-07-03 01:14:50 -0300

    criar rotas e paginas

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import {Header } from './header';
-import {Footer} from './footer';
-import {Main}from './main';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import SignInPage from "./SignInPage.js";
+import SignUpPage from "./SignUpPage.js";
+import HomePage from "./HomePage.js";
+import DepositPage from "./DepositPage.js";
+import WithdrawPage from "./WithdrawPage.js";
 
 
-const App =()=>{
-    return <div>
-            
-            <Header/>
-            <Main></Main>
-            <Footer/>
-        </div>;
-};
+function App(){
+    //precisa criar uma forma de manipular o token pelo corpo do app.
+    return (
+        <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<SignInPage/>}/>
+        <Route path='/singn-up' element={<SignUpPage/>}/>
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/deposit' element={<DepositPage/>}/>
+        <Route path='/withdraw' element={<WithdrawPage/>}/>
+        </Routes>
+        </BrowserRouter>
+        )}
 
 export default App;
